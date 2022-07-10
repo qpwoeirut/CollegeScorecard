23ba9648f8d759100fb9884bb57dd6b92d9f255e
qpwoeirut <stanleyzhong8@gmail.com>
2022-07-09 17:26:28 -0700

    Move api stuff to query_api.py, add US News links

--- main.py
+++ main.py
@@ -1,159 +1,17 @@
 import csv
-import hashlib
-import json
-import requests
 
-with open(".env") as f:
-    API_KEY = f.read().strip()
-
-SAT_CUTOFF = 1300
-SALARY_CUTOFF = 60000
-ENDPOINT = f"https://api.data.gov/ed/collegescorecard/v1/schools?api_key={API_KEY}&per_page=50"
-KEY_MAPPING = {
-    "school.name": "Name",
-    "school.city": "City",
-    "school.state": "State",
-    "school.school_url": "Website",
-    "school.locale": "Setting",
-    "latest.cost.tuition.in_state": "In-state Tuition",
-    "latest.cost.tuition.out_of_state": "Out-of-state Tuition",
-    "latest.student.size": "Undergrad Size",
-    "latest.earnings.6_yrs_after_entry.median": "6-year Median Salary",
-    "latest.completion.consumer_rate": "Graduation Rate",
-    "latest.admissions.admission_rate.consumer_rate": "Admission Rate",
-    "latest.academics.program_percentage.computer": "% of CS Degrees",
-
-    "latest.admissions.sat_scores.25th_percentile.critical_reading": "SAT 25th % Critical Reading",
-    "latest.admissions.sat_scores.75th_percentile.critical_reading": "SAT 75th % Critical Reading",
-    "latest.admissions.sat_scores.25th_percentile.math": "SAT 25th % Math",
-    "latest.admissions.sat_scores.75th_percentile.math": "SAT 75th % Math",
-    "latest.admissions.sat_scores.25th_percentile.writing": "SAT 25th % Writing",
-    "latest.admissions.sat_scores.75th_percentile.writing": "SAT 75th % Writing",
-    "latest.admissions.sat_scores.average.overall": "SAT Average",
-}
+from query_api import filter_schools, get_school_list, transform_school, KEY_MAPPING
 
 
-def make_request(query: str) -> dict:
-    url = f"{ENDPOINT}&{query}"
-    resp = requests.get(url)
-    assert resp.status_code == 200, resp.text
-    data = resp.json()
-    assert "error" not in data, data["error"]
-    return data
-
-
-def make_query(filters: dict[str,], fields: list = None) -> list[dict]:
-    filters_str = '&'.join([f"{k}={v}" for k, v in filters.items()])
-    fields_str = f"fields={','.join(fields)}" if fields else ""
-    query = f"{filters_str}&{fields_str}"
-    query_hash = hashlib.sha1(query.encode()).hexdigest()  # use hash since query string is too long
-
-    try:
-        with open(f"cache/{query_hash}.json") as f:
-            return json.loads(f.read().strip())
-    except FileNotFoundError:
-        pass
-
-    data = make_request(query)
-    metadata = data["metadata"]
-    total, per_page = metadata["total"], metadata["per_page"]
-
-    all_results = []  # makes a repeat request, but should be okay
-    for page in range((total + per_page - 1) // per_page):
-        print(f"Fetching page {page}")
-        data = make_request(f"{query}&page={page}")
-        results = data["results"]
-        all_results.extend(results)
-
-    with open(f"cache/{query_hash}.json", "w+") as f:
-        f.write(json.dumps(all_results))
-    return all_results
-
-
-def translate_locale_value(locale: int) -> str:
-    locale = (locale // 10) - 1
-    return ["city", "suburb", "town", "rural"][locale]
-
-
-def get_school_list(filters: dict[str,]) -> list[dict[str,]]:
-    return make_query(
-        filters,
-        [
-            "school.name",
-            "school.city", "school.state",
-            "school.school_url",
-            "school.type",
-            "school.locale",
-
-            "latest.cost.tuition.in_state", "latest.cost.tuition.out_of_state",
-
-            "latest.student.size",
-
-            "latest.earnings.6_yrs_after_entry.median",
-            # "latest.earnings.7_yrs_after_entry.mean_earnings",
-            # "latest.earnings.8_yrs_after_entry.mean_earnings", "latest.earnings.8_yrs_after_entry.median_earnings",
-            # "latest.earnings.9_yrs_after_entry.mean_earnings", "latest.earnings.10_yrs_after_entry.median",
-
-            "latest.completion.consumer_rate",
-            # "latest.completion.title_iv.completed_by.4yrs", "latest.completion.title_iv.completed_by.6yrs",
-
-            "latest.admissions.test_requirements",
-            "latest.admissions.admission_rate.consumer_rate",
-            "latest.admissions.sat_scores",  # only took the SAT, so that's all I care about
-
-            "latest.academics.program.degree.computer",
-            "latest.academics.program_percentage.computer",
-
-            # "latest.programs.cip_4_digit"  # Stanford's CS is 1107
-        ]
-    )
-
-
-def filter_schools(schools: list[dict[str,]]) -> list[dict[str,]]:
-    # API can't filter based on these values
-    filtered_schools = []
-    for school in schools:  # could be done with a list comprehension, but using loops allows for printing stuff out
-        if school["latest.academics.program_percentage.computer"] == 0:
-            # print(f"Filtered {school['school.name']}.".ljust(70, '.') + " no Bachelors program for CS")
-            continue
-
-        median_earnings_6_yrs = school["latest.earnings.6_yrs_after_entry.median"]
-        if median_earnings_6_yrs is None:
-            # print(f"Filtered {school['school.name']}.".ljust(70, '.') + " no median earnings data")
-            continue
-        if median_earnings_6_yrs < SALARY_CUTOFF:
-            # print(
-            #     f"Filtered {school['school.name']}.".ljust(70, '.') +
-            #     f" median earnings 6 years after entry (${median_earnings_6_yrs}) is < ${SALARY_CUTOFF}")
-            continue
-
-        sat_score = school["latest.admissions.sat_scores.average.overall"]
-        if sat_score is not None and sat_score < SAT_CUTOFF:
-            # print(f"Filtered {school['school.name']}.".ljust(70, '.') + f" SAT average ({sat_score}) is < {SAT_CUTOFF}")
-            continue
-
-        filtered_schools.append(school)
-    return filtered_schools
-
-
-def transform_school(school: dict[str,]) -> dict[str,]:
-    school = {KEY_MAPPING[k]: v for k, v in school.items() if k in KEY_MAPPING}
-
-    school["Setting"] = translate_locale_value(school["Setting"])
-    if not school["Website"].startswith("http"):
-        school["Website"] = "https://" + school["Website"]
-    return school
-
-
-def general_search() -> list[dict[str,]]:
+def general_search() -> dict[str, dict[str,]]:
     schools = get_school_list({"school.operating": 1, "latest.student.size__range": "500.."})
     schools.sort(key=lambda s: s["school.name"])
-    filtered = [transform_school(school) for school in filter_schools(schools)]
-    return filtered
+    schools = [transform_school(school) for school in filter_schools(schools)]
+    return {school["Name"]: school for school in schools}
 
 
-def search_from_list(names: list[str]) -> list[dict[str,]]:
-    schools = []
+def search_from_list(names: list[str]) -> dict[str, dict[str,]]:
+    schools = {}
     for name in names:
         results = get_school_list({"school.name": name})
         # the search is autocompleted, so make sure we get the value we want
@@ -161,8 +19,8 @@ def search_from_list(names: list[str]) -> list[dict[str,]]:
             school = results[0] if len(results) == 1 else [r for r in results if r["school.name"] == name][0]
         except IndexError:
             raise ValueError(f"{name} not found. found {[s['school.name'] for s in results]} instead")
-        schools.append(school)  # couldn't get multivalue filter working for school.name
-    return [transform_school(school) for school in schools]
+        schools[name] = transform_school(school)  # couldn't get multivalue filter working for school.name
+    return schools
 
 
 def print_schools(schools: list[dict[str,]]):
@@ -174,23 +32,34 @@ def print_schools(schools: list[dict[str,]]):
         print('=' * 80, '\n')
 
 
-def save_schools(filename: str, schools: list[dict[str,]]):
+def add_us_news_link(us_news_dict: dict[str, str], schools: dict[str, dict[str, ]]) -> dict[str, dict[str, ]]:
+    for k in schools.keys():
+        schools[k]["US News Link"] = f"https://www.usnews.com/best-colleges/{us_news_dict[k]}"
+    return schools
+
+
+def save_schools(filename: str, schools: dict[str, dict[str,]], additional_fields: list):
     with open(filename, "w+") as csv_file:
-        field_names = KEY_MAPPING.values()
-        writer = csv.DictWriter(csv_file, field_names)
+        field_names = list(KEY_MAPPING.values()) + additional_fields
+        field_names.remove("Website")
+
+        writer = csv.DictWriter(csv_file, field_names, extrasaction="ignore")
         writer.writeheader()
-        for school in schools:
+        for school in schools.values():
+            school["Name"] = f"""=HYPERLINK("{school["Website"]}", "{school["Name"]}")"""
             writer.writerow(school)
 
 
 def main():
-    with open("school_list.txt") as f:
-        school_list = [line.strip() for line in f]
-    schools = search_from_list(school_list)
-    save_schools("ranking.csv", schools)
+    with open("school_list.csv") as f:
+        reader = csv.DictReader(f)
+        school_dict = {row["Name"]: row["US N&W ID"] for row in reader}
+    schools = search_from_list(list(school_dict.keys()))
+    schools = add_us_news_link(school_dict, schools)
+    save_schools("ranking.csv", schools, ["US News Link"])
 
     schools = general_search()
-    save_schools("general.csv", schools)
+    save_schools("general.csv", schools, [])
 
 
 if __name__ == '__main__':
